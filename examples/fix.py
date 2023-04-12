6b2b4a29f325342a25414061d87a68f1f85a16ea
Pete Gadomski <pete.gadomski@gmail.com>
2023-04-12 11:00:32 -0600

    fix: example script, formatting

--- examples/fix.py
+++ examples/fix.py
@@ -1,12 +1,13 @@
 import json
 import sys
 
-import antimeridian
 import shapely.geometry
+
+import antimeridian
 
 with open(sys.argv[1]) as f:
     data = json.load(f)
 if "geometry" in data:  # handle GeoJSON features
     data = data["geometry"]
-polygons = antimeridian.close_polygon(shapely.geometry.shape(data))
+polygons = antimeridian.fix_polygon(shapely.geometry.shape(data))
 print(json.dumps(shapely.geometry.mapping(polygons), indent=4))
