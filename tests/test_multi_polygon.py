4eeb57762dff42d6a2c415646e569d80354ff2b2
Pete Gadomski <pete.gadomski@gmail.com>
2023-04-14 06:11:20 -0600

    fix, tests: multi polygon test

--- tests/test_multi_polygon.py
+++ tests/test_multi_polygon.py
@@ -1,15 +1,14 @@
-from typing import Callable
-
 import shapely.geometry
 from shapely.geometry import MultiPolygon
 
 import antimeridian
-from antimeridian import GeoInterface
+
+from .conftest import Reader
 
 
 def test_multi_polygon(
-    read_input: Callable[[str], GeoInterface],
-    read_output: Callable[[str], GeoInterface],
+    read_input: Reader,
+    read_output: Reader,
 ) -> None:
     input = read_input("multi-split")
     assert isinstance(input, MultiPolygon)
@@ -20,7 +19,7 @@ def test_multi_polygon(
     assert fixed.normalize() == output.normalize()
 
 
-def test_fix_shape(read_input: Callable[[str], GeoInterface]) -> None:
+def test_fix_shape(read_input: Reader) -> None:
     # Just a smoke test
     input = shapely.geometry.mapping(read_input("multi-split"))
     antimeridian.fix_shape(input)
