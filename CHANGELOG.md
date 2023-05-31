8e7abfeb5d4ca4af420a2f76eda3745f449e825d
Pete Gadomski <pete.gadomski@gmail.com>
2023-05-31 07:47:50 -0600

    feat: add bbox function

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 
 ## [Unreleased]
 
+### Added
+
+- Calculate spec-conforming bounding boxes ([#35](https://github.com/gadomski/antimeridian/pull/35))
+
 ## [0.2.4] - 2023-05-12
 
 ### Fixed
--- tests/test_multi_polygon.py
+++ tests/test_multi_polygon.py
@@ -1,17 +1,26 @@
 import antimeridian
+import pytest
 import shapely.geometry
 from shapely.geometry import MultiPolygon
 
 from .conftest import Reader
 
 
+@pytest.mark.parametrize(
+    ("name"),
+    [
+        "multi-split",
+        "multi-no-antimeridian",
+    ],
+)
 def test_multi_polygon(
     read_input: Reader,
     read_output: Reader,
+    name: str,
 ) -> None:
-    input = read_input("multi-split")
+    input = read_input(name)
     assert isinstance(input, MultiPolygon)
-    output = read_output("multi-split")
+    output = read_output(name)
     assert isinstance(output, MultiPolygon)
     fixed = antimeridian.fix_multi_polygon(input)
     assert fixed.is_valid
