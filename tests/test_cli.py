9a05d17843e4c7609dff219d85a03d3b90cb6115
Pete Gadomski <pete.gadomski@gmail.com>
2023-06-30 06:18:28 -0600

    fix, tests: warnings

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -9,7 +9,7 @@ pytest.importorskip("click")
 
 def test_fix(script_runner: ScriptRunner, input_path: Callable[[str], Path]) -> None:
     path = input_path("simple")
-    result = script_runner.run("antimeridian", "fix", str(path))
+    result = script_runner.run(["antimeridian", "fix", str(path)])
     assert result.success
 
 
@@ -17,5 +17,5 @@ def test_segment(
     script_runner: ScriptRunner, input_path: Callable[[str], Path]
 ) -> None:
     path = input_path("simple")
-    result = script_runner.run("antimeridian", "segment", str(path))
+    result = script_runner.run(["antimeridian", "segment", str(path)])
     assert result.success
