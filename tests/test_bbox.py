56a8099ffea792c2a93500c3b34be710adaf9d56
Pete Gadomski <pete.gadomski@gmail.com>
2024-09-04 06:26:45 -0600

    --wip-- [skip ci]

--- tests/test_bbox.py
+++ tests/test_bbox.py
@@ -13,6 +13,15 @@ from .conftest import Reader
         ("split", [170, 40, -170, 50]),
         ("multi-no-antimeridian", [90, 10, 100, 50]),
         ("north-pole", [-180, 40, 180, 90]),
+        (
+            "issues-134",
+            [
+                179.96779787822723,
+                -19.044135782844712,
+                -179.77058698198195,
+                -18.555752850452095,
+            ],
+        ),
     ],
 )
 def test_bbox(read_output: Reader, name: str, expected: List[float]) -> None:
